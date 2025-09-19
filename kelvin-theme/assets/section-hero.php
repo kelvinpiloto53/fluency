8884641f0458be2a0afb20f9ea4819807df6520f
Kelvin Piloto <kelvin.uadvert@gmail.com>
2025-09-19 01:59:07 -0300

    Seção 1 Banner
    Atualização do estilo da  primeira seção do banner e formulário de captação de leeds

---
+++ kelvin-theme/assets/section-hero.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template para a seção Hero da página inicial.
+ *
+ * @package Kelvin_Theme
+ */
+?>
+
+<section class="hero-section blur-in delay-3" style="background-image: url('<?php echo get_field('banner_site'); ?>');">
+    <div class="container formsec">
+        <h1 class="hero-title animate fade-up delay-1">Curso online de inglês <br>em <span>São Paulo</span></h1>
+        <div class="formulario">
+            
+        </div>
+    </div>
+</section>
+<section class="hero-products">
+    <div class="container grid-lg-4 grid-sm-1">
+       <div>TESTE 1</div>
+       <div>TESTE 2</div>
+       <div>TESTE 3</div>
+       <div>TESTE 4</div>
+    </div>
+</section>
+
